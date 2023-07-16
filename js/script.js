b726a95994d5f11d5f5357b6ef55e03188ceb9f4
Caro Cenoz <macarocen@gmail.com>
2023-07-15 22:54:17 -0300

    first

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const firstProduct = {name : "microwave", price : 2000};
+const secondProduct = {name :"trashcan", price : 1000};
+const thirdProduct = {name : "fridge", price : 3000};
+const fourthProduct = {name : "toaster", price : 1500};
+
+let theCart = [];
+
+function checkCart(){
+    /*CONTAR PRODUCTOS EN CARRITO*/
+    console.log(theCart.length);
+}
+
+function getTotalPriceWithDiscount(){
+    let totalPrice = 0;
+    for(let i = 0; i < theCart.length; i++){
+        totalPrice += theCart[i].price
+    }
+    if(theCart.length >= 3){
+        totalPrice = totalPrice * 0.90;
+    }
+    return totalPrice
+}
+
+console.log(getTotalPriceWithDiscount())
+
+
+function addPush(){
+    theCart.push(firstProduct);
+    console.log("¡Se agregó " + firstProduct.name + " al carrito!");
+    theCart.push(secondProduct);
+    console.log("¡Se agregó " + secondProduct.name + " al carrito!");
+    theCart.push(thirdProduct);
+    console.log("¡Se agregó " + thirdProduct.name + " al carrito!");
+    theCart.push(fourthProduct);
+    console.log("¡Se agregó " + fourthProduct.name + " al carrito!");
+}
