8f1f668c4e830dfce7ebd6aa452187b9c05c9bca
Caro Cenoz <macarocen@gmail.com>
2023-07-29 18:45:03 -0300

    array, arreglo de push, agrego find, agrego filter

--- js/script.js
+++ js/script.js
@@ -1,38 +1,55 @@
-const firstProduct = {name : "microwave", price : 2000};
-const secondProduct = {name :"trashcan", price : 1000};
-const thirdProduct = {name : "fridge", price : 3000};
-const fourthProduct = {name : "toaster", price : 1500};
+//ARRAY
+const products = [
+  { id: 30, name: "microwave", price: 2000 },
+  { id: 108, name: "trashcan", price: 1000 },
+  { id: 5041, name: "fridge", price: 3000 },
+  { id: 2, name: "microwave", price: 1500 },
+];
 
-let theCart = [];
+let sentId = 5041;
+const theCart = [];
 
-function checkCart(){
-    /*CONTAR PRODUCTOS EN CARRITO*/
-    console.log(theCart.length);
+//FIND
+let productFind = products.find(element => element.id == sentId);
+console.log(productFind)
+
+//FILTER
+const productFilter = products.filter(element => element.price > 2500);
+console.log(productFilter);
+
+//DECLARO FUNCION CONTAR ELEMENTOS EN CARRITO
+function checkCart() {
+  console.log(theCart.length);
 }
 
-function getTotalPriceWithDiscount(){
-    let totalPrice = 0;
-    for(let i = 0; i < theCart.length; i++){
-        totalPrice += theCart[i].price
-    }
-    if(theCart.length >= 3){
-        totalPrice = totalPrice * 0.90;
-    }
-    return totalPrice
+//DECLARO FUNCION DESCUENTOS
+function getTotalPriceWithDiscount() {
+  let totalPrice = 0;
+  for (let i = 0; i < theCart.length; i++) {
+    totalPrice += theCart[i].price;
+  }
+  if (theCart.length >= 3) {
+    totalPrice = totalPrice * 0.9;
+  }
+  return totalPrice;
 }
 
-function addPush(){
-    theCart.push(firstProduct);
-    console.log("¡Se agregó " + firstProduct.name + " al carrito!");
-    theCart.push(secondProduct);
-    console.log("¡Se agregó " + secondProduct.name + " al carrito!");
-    theCart.push(thirdProduct);
-    console.log("¡Se agregó " + thirdProduct.name + " al carrito!");
-    theCart.push(fourthProduct);
-    console.log("¡Se agregó " + fourthProduct.name + " al carrito!");
+//DECLARO FUNCION AGREGAR ELEMENTOS AL CARRITO (CON PUSH)
+function addPush() {
+  theCart.push(products[0]);
+  console.log("¡Se agregó " + products[0].name + " al carrito!");
+  theCart.push(products[1]);
+  console.log("¡Se agregó " + products[1].name + " al carrito!");
+  theCart.push(products[2]);
+  console.log("¡Se agregó " + products[2].name + " al carrito!");
+  theCart.push(products[3]);
+  console.log("¡Se agregó " + products[3].name + " al carrito!");
 }
 
 
+
+
+//LLAMADO FUNCION PUSH
 addPush();
 const total = getTotalPriceWithDiscount();
-console.log(total)
+console.log(total);
---
+++ notas.js
@@ -0,0 +1,23 @@
+//ARRAY
+
+const products = [
+  {id: 30, name: "microwave", price: 2000 },
+  {id: 2, name: "trashcan", price: 1000 },
+  {id: 5041, name: "fridge", price: 3000 },
+  {id: 108, name: "microwave", price: 1500 },
+];
+
+const theCart = [];
+
+//mostrar productos en el html
+
+let sentId = 5041;
+
+let productFind = products.find(element => element.id == sentId);
+console.log(productFind)
+
+const productFilter = products.filter(element => element.price > 2500);
+console.log(productFilter);
+
+//const productFilter1 = products.filter(element => element.price > 2500);
+//console.log(productFilter1);
