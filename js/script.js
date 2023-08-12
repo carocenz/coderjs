434f4c32ce07dc607e524ed0f8da8d622d5176f4
Caro Cenoz <macarocen@gmail.com>
2023-08-12 19:17:25 -0300

    se agregan eventos, json y storage, se modifica el html con el buscador y se agrega un css que después va a modificar el estilo del proyecto

--- js/script.js
+++ js/script.js
@@ -1,55 +1,95 @@
-//ARRAY
+//ARRAY de objetos
 const products = [
   { id: 30, name: "microwave", price: 2000 },
   { id: 108, name: "trashcan", price: 1000 },
   { id: 5041, name: "fridge", price: 3000 },
-  { id: 2, name: "microwave", price: 1500 },
+  { id: 2, name: "toaster", price: 1500 },
 ];
 
-let sentId = 5041;
+//variables del buscador
+let nameProduct = "";
+let priceProduct = 0;
+const productNameElement = document.querySelector(".add_product");
+const productPriceElement = document.querySelector(".add_price");
+const addButton = document.getElementById("button");
+
+//event listener del input de name
+productNameElement.addEventListener("input", (e) => {
+  nameProduct = e.target.value;
+});
+
+//event listener del input de price
+productPriceElement.addEventListener("input", (e) => {
+  priceProduct = e.target.value;
+});
+
+//event listener del boton add para que pueda generar un id nuevo (ver funcion generateUniqueId) y único cada vez que se agrega un producto (ver push)
+addButton.addEventListener("click", (e) => {
+  const newProduct = {
+    id: generateUniqueId(),
+    name: nameProduct,
+    price: priceProduct,
+  };
+  //push para agregar el nuevo producto ingresado en el input luego del event listener del boton
+  products.push(newProduct);
+  //json para convertir el objeto y que el storage pueda agregarlo de la forma que yo quería
+  const productsString = JSON.stringify(newProduct);
+  localStorage.setItem(nameProduct, productsString);
+});
+
+//funcion que genera el id unico por producto ingresado
+function generateUniqueId() {
+  return Date.now();
+}
+
+let sentId = 2;
 const theCart = [];
 
-//FIND
-let productFind = products.find(element => element.id == sentId);
-console.log(productFind)
+function getProductByName(name) {
+  const result = products.filter((element) => element.name === name);
+  if (result.length > 0) {
+    console.log(result[0].id);
+  } else {
+    console.log("Producto no encontrado.");
+  }
+}
 
-//FILTER
-const productFilter = products.filter(element => element.price > 2500);
-console.log(productFilter);
-
-//DECLARO FUNCION CONTAR ELEMENTOS EN CARRITO
+//declaro funcion para contar cuantos elementos hay en el carrito
 function checkCart() {
   console.log(theCart.length);
 }
 
-//DECLARO FUNCION DESCUENTOS
-function getTotalPriceWithDiscount() {
-  let totalPrice = 0;
-  for (let i = 0; i < theCart.length; i++) {
-    totalPrice += theCart[i].price;
-  }
-  if (theCart.length >= 3) {
-    totalPrice = totalPrice * 0.9;
+//declaro funcion para hacer el descuento
+function getTotalPriceWithDiscount(cart) {
+  let totalPrice = cart.reduce((total, item) => total + item.price, 0);
+  if (cart.length >= 3) {
+    totalPrice *= 0.9;
   }
   return totalPrice;
 }
 
-//DECLARO FUNCION AGREGAR ELEMENTOS AL CARRITO (CON PUSH)
-function addPush() {
-  theCart.push(products[0]);
-  console.log("¡Se agregó " + products[0].name + " al carrito!");
-  theCart.push(products[1]);
-  console.log("¡Se agregó " + products[1].name + " al carrito!");
-  theCart.push(products[2]);
-  console.log("¡Se agregó " + products[2].name + " al carrito!");
-  theCart.push(products[3]);
-  console.log("¡Se agregó " + products[3].name + " al carrito!");
+//declaro funcion agregar elementos al carrito (con push)
+function addToCart(product) {
+  theCart.push(product);
+  console.log("¡Se agregó " + product.name + " al carrito!");
 }
 
+//llamo a la funcion agregar elementos al carrito
+addToCart(products[0]);
+addToCart(products[1]);
+addToCart(products[2]);
+addToCart(products[3]);
 
+//find
+let productFind = products.find((element) => element.id == sentId);
+console.log(productFind);
 
+//filter
+const productFilter = products.filter((element) => element.price > 2500);
+console.log(productFilter);
 
-//LLAMADO FUNCION PUSH
-addPush();
-const total = getTotalPriceWithDiscount();
+//llamado funcion push
+const total = getTotalPriceWithDiscount(theCart);
 console.log(total);
+
+getProductByName("microwave");
--- notas.js
+++ notas.js
@@ -1,23 +1,28 @@
 //ARRAY
+//function getProductByName() {
 
-const products = [
-  {id: 30, name: "microwave", price: 2000 },
-  {id: 2, name: "trashcan", price: 1000 },
-  {id: 5041, name: "fridge", price: 3000 },
-  {id: 108, name: "microwave", price: 1500 },
-];
+//let name = prompt("Nombre del producto");
 
-const theCart = [];
+//let result = products.filter(element => element.name == name);
+//console.log(result);
+//}
+
+//const products = [
+  //{id: 30, name: "microwave", price: 2000 },
+  //{id: 2, name: "trashcan", price: 1000 },
+  //{id: 5041, name: "fridge", price: 3000 },
+  //{id: 108, name: "microwave", price: 1500 },
+//];
+
+//const theCart = [];
 
 //mostrar productos en el html
 
-let sentId = 5041;
+//let sentId = 5041;
 
-let productFind = products.find(element => element.id == sentId);
-console.log(productFind)
+//let productFind = products.find(element => element.id == sentId);
+//console.log(productFind)
 
-const productFilter = products.filter(element => element.price > 2500);
-console.log(productFilter);
+//const productFilter = products.filter(element => element.price > 2500);
+//console.log(productFilter);
 
-//const productFilter1 = products.filter(element => element.price > 2500);
-//console.log(productFilter1);
