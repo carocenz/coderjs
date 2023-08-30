9a28ee5db2f01fa12176f70c4aa8ce236ca52261
Caro Cenoz <macarocen@gmail.com>
2023-08-29 21:10:12 -0300

    se agrega api, se mejora la sintaxis, se agregan promesas con fetch y la libreria. se agregan addevents para lograr mejoras en la interaccion con el dom.

--- js/script.js
+++ js/script.js
@@ -6,9 +6,21 @@ const products = [
   { id: 2, name: "toaster", price: 1500 },
 ];
 
-//variables del buscador
+const productsFetch = [];
+
+//variables
 let nameProduct = "";
 let priceProduct = 0;
+
+const shopperBtn = document.getElementById("shopper_btn");
+const sellerBtn = document.getElementById("seller_btn");
+
+const shopperPage = document.getElementById("shopper_page");
+const sellerPage = document.getElementById("seller_page");
+
+const productsList = document.getElementById("products_list");
+const sellerList = document.getElementById("seller_list");
+
 const productNameElement = document.querySelector(".add_product");
 const productPriceElement = document.querySelector(".add_price");
 const addButton = document.getElementById("button");
@@ -35,6 +47,8 @@ addButton.addEventListener("click", (e) => {
   //json para convertir el objeto y que el storage pueda agregarlo de la forma que yo quería
   const productsString = JSON.stringify(newProduct);
   localStorage.setItem(nameProduct, productsString);
+
+  updateSellerList();
 });
 
 //funcion que genera el id unico por producto ingresado
@@ -93,3 +107,88 @@ const total = getTotalPriceWithDiscount(theCart);
 console.log(total);
 
 getProductByName("microwave");
+updateSellerList();
+
+//API y fetch - forEach
+const fetchProductsFromAPI = () => {
+  if (productsFetch.length > 0) return;
+  fetch("https://fakestoreapi.com/products?limit=10")
+    .then((res) => res.json())
+    .then((json) => {
+      json.forEach((product) => {
+        productsFetch.push(...json);
+        createProductElement(product);
+      });
+    });
+};
+
+//eventlistener de boton de shopper
+shopperBtn.addEventListener("click", () => {
+  fetchProductsFromAPI();
+  sellerBtn.style.backgroundColor = "inherit";
+  shopperBtn.style.backgroundColor = "pink";
+  sellerPage.style.display = "none";
+  shopperPage.style.display = "block";
+});
+
+//eventListener de boton de seller
+sellerBtn.addEventListener("click", () => {
+  shopperBtn.style.backgroundColor = "inherit";
+  sellerBtn.style.backgroundColor = "pink";
+  shopperPage.style.display = "none";
+  sellerPage.style.display = "block";
+});
+
+//create de productos de api
+const createProductElement = (product) => {
+  const productBox = document.createElement("div");
+  const title = document.createElement("h3");
+  const price = document.createElement("p");
+  const image = document.createElement("img");
+
+  title.textContent = product.title;
+  price.textContent = product.price;
+  image.src = product.image;
+
+  //append
+  productBox.append(title, price, image);
+
+  //eventListener de productBox y toastify
+  productBox.addEventListener("click", () => {
+    Toastify({
+      text: product.title + " added successfully",
+      duration: 3000,
+      newWindow: true,
+      close: true,
+      gravity: "top",
+      position: "center",
+      stopOnFocus: true,
+      style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+      },
+    }).showToast();
+  });
+
+  //appendChild para hacer la lista
+  productsList.appendChild(productBox);
+};
+
+//funcion para resetear la lista cada vez que se aprieta add
+function updateSellerList() {
+  sellerList.innerHTML = "";
+  for (const prod of products) {
+    const productBox = document.createElement("div");
+    const title = document.createElement("h3");
+    const price = document.createElement("p");
+
+    title.textContent = prod.name;
+    price.textContent = prod.price;
+
+    productBox.setAttribute("class", "sellerProductBox");
+    productBox.append(title, price);
+
+    sellerList.appendChild(productBox);
+    productNameElement.value = "";
+    productPriceElement.value = "";
+  }
+}
